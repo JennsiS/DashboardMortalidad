b714a2dc636d1921832d7e718f6d413df31d3e94
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-03-17 22:22:32 -0600

    Adding graphics and more cleaning

--- clean_files.r
+++ clean_files.r
@@ -1,8 +1,10 @@
 #Importacion de las librerias necesarias
 library("readxl")
+library(dplyr)
+library(stringr)
 
 #referenciando el directorio de trabajo
-setwd("C:/Users/bff_n/Documents/GitHub/Dashboard-Mortalidad/")
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 #Lectura de datos
 data_1519 <- read_excel( "b1519_renap.xlsx")
@@ -23,10 +25,16 @@ unique(data_1519$DEPARTAMENTO)
 
 #Verificando los valores unicos de las demas columnas
 unique(data_1519$SEXO)
+data_1519$SEXO[data_1519$SEXO == "I"] ="DESCONOCIDO"
+unique(data_1519$EDAD)
+data_1519$EDAD[is.na(data_1519$EDAD)] = "DESCONOCIDA"
+
 unique(data_1519$CAUSA1)
+unique(data_1519$CAUSA2)
+unique(data_1519$CAUSA3)
+unique(data_1519$CAUSA4)
+unique(data_1519$FECHA)
 data_1519 <- data_1519[,-3]
-
-
 
 #Eliminando columnas y modificando nombres para hacer coincidir bases de datos 
 data_1921
@@ -42,7 +50,13 @@ names (data_1921)[9] = "CAUSA4"
 
 unique(data_1921$DEPARTAMENTO)
 data_1921 <- subset(data_1921, DEPARTAMENTO %in% departamentos_validos)
-unique(data_1921$DEPARTAMENTO)
+
+#Cambiando los valores faltantes por "DESCONOCIDOS"
+unique(data_1921$SEXO)
+data_1921$SEXO[is.na(data_1921$SEXO)] = "DESCONOCIDO"
+
+unique(data_1921$EDAD)
+data_1921$EDAD[is.na(data_1921$EDAD)] = "DESCONOCIDA"
 
 data_1921 <- data_1921 %>% filter(FECHA >= "2015-01-01")
 
@@ -54,10 +68,23 @@ all_data <- all_data[,c("ID", names(all_data)[names(all_data) != "ID"])]
 
 all_data[['FECHA']] <- strptime(all_data[['FECHA']], format = "%Y-%m-%d")
 
-#all_data[is.na(all_data)] = "DESCONOCIDA"
+
+
 unique(all_data$EDAD)
-unique(all_data$CAUSA1)
+unique(all_data$SEXO)
 unique(all_data$FECHA)
+all_data$CAUSA1[is.na(all_data$CAUSA1)] = "DESCONOCIDA"
+all_data$CAUSA1[all_data$CAUSA1=='"Ignorado'] = "DESCONOCIDA"
+all_data$CAUSA1[all_data$CAUSA1=='"Ignorado"'] = "DESCONOCIDA"
+all_data$CAUSA1[all_data$CAUSA1=='"ignorado"'] = "DESCONOCIDA"
+all_data$CAUSA1<- str_replace_all(all_data$CAUSA1, "^-", "DESCONOCIDA")
+all_data$CAUSA2[is.na(all_data$CAUSA2)] = "DESCONOCIDA"
+all_data$CAUSA3[is.na(all_data$CAUSA3)] = "DESCONOCIDA"
+all_data$CAUSA4[is.na(all_data$CAUSA4)] = "DESCONOCIDA"
+#all_data$CAUSA2<- str_replace_all(all_data$CAUSA2, "^-", "DESCONOCIDA")
+#all_data$CAUSA3<- str_replace_all(all_data$CAUSA3, "^-", "DESCONOCIDA")
+#all_data$CAUSA4<- str_replace_all(all_data$CAUSA4, "^-", "DESCONOCIDA")
 all_data
 
 write.csv(all_data,"all_data.csv", row.names = FALSE)
+
--- graphics.R
+++ graphics.R
@@ -3,11 +3,14 @@ library(dplyr)
 library(maps)
 library(plotly)
 library(hrbrthemes)
+library(rjson)
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 all_data <- read.csv(file = 'all_data.csv')
 Sys.setlocale(locale = "es_ES.UTF-8")
 
+###################Grafica muertes por grupo etario y sexo ###################
+#Colocando una columna adicional clasifique los registros en grupo etario
 grupos_etarios <- all_data %>%
   mutate(grupo_edad = case_when(
     between(EDAD,0,9) ~ "0-9",
@@ -22,23 +25,28 @@ grupos_etarios <- all_data %>%
     between(EDAD,90,99) ~ "90-99",
     between(EDAD,100,109) ~ "100-109",
     between(EDAD,110,119) ~ "110-119",
-    between(EDAD,120,150) ~ "120+"
+    between(EDAD,120,150) ~ "120+",
+    EDAD == "DESCONOCIDA" ~ "DESCONOCIDA"
   ))
 
 grupos_etarios$n <- 1
 grupos_etarios <- grupos_etarios %>% group_by(grupo_edad, SEXO) %>% summarise(n=sum(n))
+grupos_etarios$grupo_edad <- factor(grupos_etarios$grupo_edad, levels = 
+                                      c("0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69",
+                                        "70-79", "80-89", "90-99", "100-109", "110-119", "120+", "DESCONOCIDA"))
 
-#data_edad <-na.omit(all_data)
-grupos_etarios %>% group_by(gr=cut(all_data, breaks= seq(0, 123, by = 10)) )
-grupos_etarios
-sexo <- all_data %>% count(SEXO)
-registros <- data_edad
-data <- data_edad
 
-fig <- plot_ly(grupos_etarios%>% filter(SEXO=="F"),x = ~grupo_edad, y = ~n, type = 'bar', name = "femenino") %>%
-  add_trace(grupos_etarios%>% filter(SEXO=="M"),x = ~grupo_edad, y = ~n, name = "masculino", color = "#16a085") %>%
-  layout(xaxis = list(title = 'x'),
-         yaxis = list(title = 'y'),
+mujeres <- grupos_etarios%>% filter(SEXO=="F")
+hombres <- grupos_etarios%>% filter(SEXO=="M")
+mujeres$n
+
+
+fig <- plot_ly(grupos_etarios, x = ~grupo_edad, y= ~n, type = 'bar', name = "TOTAL")%>%
+  add_trace(mujeres,x = mujeres$grupo_edad, y = mujeres$n,  name = "femenino", color = "#EB5160") %>%
+  add_trace(hombres,x = hombres$grupo_edad, y = hombres$n, name = "masculino", color = "#16a085") %>%
+  layout(xaxis = list(title = 'Grupos etarios'),
+         yaxis = list(title = 'Cantidad de muertes'),
+         barmode = 'group',
          plot_bgcolor='#e5ecf6',
          xaxis = list(
            zerolinecolor = '#ffff',
@@ -48,18 +56,15 @@ fig <- plot_ly(grupos_etarios%>% filter(SEXO=="F"),x = ~grupo_edad, y = ~n, type
            zerolinecolor = '#ffff',
            zerolinewidth = 2,
            gridcolor = 'ffff'))
-
-
 fig
+
+
+#####################Grafica de muertes por fecha#############################
 
 muertes_fecha <- all_data %>% count(FECHA)
 muertes_fecha$FECHA
 muertes_fecha$n
 
 fig <- plot_ly(muertes_fecha, x = ~FECHA, y = ~n, type = 'scatter', mode = 'lines')
-
-
 fig
-
-
 
